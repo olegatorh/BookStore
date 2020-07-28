fe81c7d796d3b98db810f6e1162284817115d748
olegatorh <herston4ik@gmail.com>
2020-07-28 21:41:33 +0300

    make git rep beauty

--- BookStoreVenv/run.py
+++ BookStoreVenv/run.py
@@ -1,6 +1,5 @@
-from app import app, db
-from app.models import Posts, DescriptionOfBook
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Posts': Posts, 'DescriptionOfBook': DescriptionOfBook}
+from app import app
+
+if __name__ == '__main__':
+    app.run(debug=True, host='192.168.1.15', port=9000)
